9c7425f7b708da9265223b8cac8a17010f31b46f
RivaniaGomes <rivaniagomes@gmail.com>
2023-02-11 13:31:40 -0300

    Sessão 3: itens 5 e 6

--- vue-basico/src/views/AboutView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>Hello word</h1>
-  </div>
-</template>
---
+++ vue-basico/src/views/ControleDeClientes.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1>{{mensagem}}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ControleDeClientes',
+    data(){
+      return{
+       mensagem: 'Estou na tela Controle De Clientes' 
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: red;
+  }
+  </style>
