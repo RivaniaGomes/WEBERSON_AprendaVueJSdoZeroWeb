b1fbb0762bf714c2123b32fc2f6d73d3e8ab6bb8
RivaniaGomes <rivaniagomes@gmail.com>
2023-02-16 15:15:38 -0300

    27. Atenticação - Parte 1

--- vue-basico/src/views/Login.vue
+++ vue-basico/src/views/Login.vue
@@ -24,6 +24,9 @@
 <script>
 import InputLogin from '../components/input/InputLogin.vue'
 import Button from '../components/button/Button'
+import Usuario from '@/models/Usuario'
+import usuarioService from '@/services/usuario-service'
+
 export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'Login',
@@ -33,14 +36,28 @@ export default {
     },
     data(){
         return{
-            usuario: {
-                email: '',
-                senha: ''
-            }
+            usuario: new Usuario()
         }
     },
     methods: {
         login(){
+
+            if (!this.usuario.modeloValidoLogin()) {
+                this.$swal({
+                    icon: 'warning',
+                    title: 'O email e senha são obrigatório!',
+                    confirmButtonColor: '#FF3D00',
+                    animate: true,
+                });
+                return;
+            }
+            usuarioService.login(this.usuario.email, this.usuario.senha)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
             //criar logica para acessar o sistema
             //awui enviaria os dados de login pra api
             //se tudo ok segue para tela de controle de produtos
