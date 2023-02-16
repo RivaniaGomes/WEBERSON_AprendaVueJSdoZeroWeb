9e73ae863e939c71c203c7d443b61446540a64a0
RivaniaGomes <rivaniagomes@gmail.com>
2023-02-16 12:33:06 -0300

    24.Trabalhando com eventos

--- vue-basico/src/views/ControleDeProdutos.vue
+++ vue-basico/src/views/ControleDeProdutos.vue
@@ -1,161 +1,85 @@
 <template>
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12">
-          <h2 class="titulo">Produtos</h2>
-          <hr/>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h2 class="titulo">Produtos</h2>
+        <hr />
       </div>
-        <div class="row sub-container">
-          <div class="col-sm-2">
-              <Button :callback="adicionarProduto" value="Adicionar"></Button>
-          </div>
-          <div class="col-sm-10">
-            <a @click="verProdutosEmCards" class="float-right ver-em-cards">Ver em cards</a>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-sm-12">
-              <table class="table table-hover">
-                <thead>
-                  <tr>
-                    <th>Código</th>
-                    <th>Nome</th>
-                    <th>Quantidade</th>
-                    <th>Valor</th>
-                    <th>Data de Cadastro</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="item in produtos" :key="item.id">
-                    <td>{{ item.id }}</td>
-                    <td>{{ item.nome }}</td>
-                    <td>{{ item.quantidadeEstoque }}</td>
-                    <td>{{ item.valor | real }}</td>
-                    <td>{{ item.dataCadastro | data}}</td>
-                    <td>
-                      <i @click="editarProduto(item)" class="fas fa-pencil-alt icones-tabela"></i>
-                      <i @click="excluirProduto(item)" class="fas fa-trash-alt icones-tabela"></i>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-          </div>
-        </div>
-
+    </div>
+    <div class="row sub-container">
+      <div class="col-sm-2">
+        <Button :callback="adicionarProduto" value="Adicionar"></Button>
       </div>
-  </template>
+      <div class="col-sm-10">
+        <a @click="verProdutosEmCards" class="float-right ver-em-cards">Ver em cards</a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th>Código</th>
+              <th>Nome</th>
+              <th>Quantidade</th>
+              <th>Valor</th>
+              <th>Data de Cadastro</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in produtos" :key="item.id">
+              <td>{{ item.id }}</td>
+              <td>{{ item.nome }}</td>
+              <td>{{ item.quantidadeEstoque }}</td>
+              <td>{{ item.valor | real }}</td>
+              <td>{{ item.dataCadastro | data }}</td>
+              <td>
+                <i @click="editarProduto(item)" class="fas fa-pencil-alt icones-tabela"></i>
+                <i @click="excluirProduto(item)" class="fas fa-trash-alt icones-tabela"></i>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
 .
-  <script>
-  import produtoService from '../services/produto-service';
-  import Button from '../components/button/Button';
-  import Produto from '@/models/Produto';
-  import ConversorDeData from '../utils/conversor-data'
-  import ConversorMonetario from '../utils/conversor-monetario'
+<script>
+import Button from '../components/button/Button';
+import ProdutoMixin from '@/mixins/produto-mixin';
 
-  export default {
-    name: 'ControleDeProdutos',
-    components: {
-      Button,
+export default {
+  name: 'ControleDeProdutos',
+  mixins: [ProdutoMixin],
+  components: {
+    Button,
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    verProdutosEmCards() {
+      this.$router.push({ name: "ListaProdutosCards" });
     },
-    filters: {
-      data(data){
-        return ConversorDeData.aplicarMascaraEmDataIso(data);
-      },
-      real(valor) {
-        return ConversorMonetario.aplicarMascaraParaRealComPrefixo(valor);
-      }
+    adicionarProduto() {
+      this.$router.push({ name: "NovoProduto" });
     },
-    data(){
-      return{
-        produtos:[]
-      }
-    },
-    mounted(){
-      this.obterTodosOsProdutos();
-    },
-    methods: {
-      verProdutosEmCards(){
-        this.$router.push({name: "ListaProdutosCards"});
-      },
-      adicionarProduto(){
-        this.$router.push({name: "NovoProduto"});
-      },
-      editarProduto(produto){
-        this.$router.push({name: "EditarProduto", params: {id: produto.id}});
-      },
-      excluirProduto(produto){
-        this.$swal({
-          icon: 'question',
-          title: 'Deseja Excluir o produto?',
-          text: `Código: ${produto.id} - Nome: ${produto.nome}`,
-          showCancelButton: true,
-          confirmButtonColor: '#FF3D00',
-          confirmButtonText: 'Sim',
-          cancelButtonText: 'Não',
-          animate: true
-        }).then((result) => {
-            if (result.isConfirmed) {
-              produtoService
-              .deletar(produto.id)
-              .then(()=>{
-                let indice = this.produtos.findIndex(p => p.id === produto.id);
-                this.produtos.splice(indice, 1);
-                this.$swal({
-                        icon: 'success',
-                        title: 'Produto excluido com sucesso!',
-                        confirmButtonColor: '#FF3D00',
-                        animate: true,
-                    });
-                //alert("Produto excluido com sucesso")
-          })
-          .catch(error =>{
-            console.log(error);
-            this.$swal({
-              icon: 'error',
-              title: 'Não foi possível excluir o produto!',
-              confirmButtonColor: '#FF3D00',
-              animate: true,
-            });
-          });
-            }
-        });
-      },
-      ordenarProdutos(a, b) {
-        return (a.id < b.id) ? -1 : (a.id > b.id) ? 1 : 0; //escolher o que vai ser ordernado, aquié o id
-      },
-      obterTodosOsProdutos(){
-        produtoService.obterTodos()
-        .then(response => {
-          let produtos = response.data.map(p => new Produto(p));
-          //reverse inverte a ordem do sort, faz o inverso do que está
-          this.produtos = produtos.sort(this.OrdenarProdutos).reverse(); //como a api já é ordernada era sõ colocar produtos.reverse()
-        })
-        .catch(error => {
-          console.log(error);
-          this.$swal({
-              icon: 'error',
-              title: 'Não foi possível obter todos os produtos!',
-              confirmButtonColor: '#FF3D00',
-              animate: true,
-            });
-        })
-      }
-    },
-  }
-  </script>
-  
-  <style scoped>
-    .icones-tabela,
-    .ver-em-cards {
-      margin: 5px;
-      cursor: pointer;
-      color: var(--cor-primaria);
-    }
-  
-    .ver-em-cards {
-      margin-top: 25px;
-    }
-  
-  </style>
+  },
+}
+</script>
+
+<style scoped>
+.icones-tabela,
+.ver-em-cards {
+  margin: 5px;
+  cursor: pointer;
+  color: var(--cor-primaria);
+}
+
+.ver-em-cards {
+  margin-top: 25px;
+}
+</style>
--- vue-basico/src/views/ListaProdutosCards.vue
+++ vue-basico/src/views/ListaProdutosCards.vue
@@ -3,48 +3,49 @@
         <div class="row">
             <div class="col-sm-12">
                 <h1 class="titulo">Cards de produtos</h1>
-                <hr>
+                <hr />
             </div>
         </div>
-
         <div class="row mt-3">
-            <Card>
-                <template v-slot:nome>
-                    produto
-                </template>
-
-                <template v-slot:valor>
-                    200
-                </template>
-
-                <template v-slot:observacao>
-                    ytttyyy produto
-                </template>
-
-                <template v-slot:acoes>
-                    <i @click="editarProduto(item)" class="fas fa-pencil-alt icones-tabela"></i>
-                    <i @click="excluirProduto(item)" class="fas fa-trash-alt icones-tabela"></i>
-                </template>
-            </Card>
+            <div v-for="produto in produtos" :key="produto.id">
+                <Card>
+                    <template v-slot:nome>{{ produto.nome }}
+                    </template>
+                    <template v-slot:valor>
+                        {{ produto.valor | real }}
+                    </template>
+                    <template v-slot:observacao>
+                        {{ produto.observacao }}
+                    </template>
+                    <template v-slot:acoes>
+                        <i @click="editarProduto(produto)" class="fas fa-pencil-alt icones-tabela"></i>
+                        <i @click="excluirProduto(produto)" class="fas fa-trash-alt icones-tabela"></i>
+                    </template>
+                </Card>
+            </div>
         </div>
-
     </div>
 </template>
 
 <script>
-import Card from '@/components/cards/Card.vue'
+import Card from "@/components/cards/Card.vue";
+import ProdutoMixin from "@/mixins/produto-mixin";
+
 export default {
-    name: 'ListaProdutosCards',
+    name: "ListaProdutosCards",
+    mixins: [ProdutoMixin],
     components: {
         Card,
     },
     data() {
-
+        return {};
     },
     methods: {
-
-    }
-}
+        trocarImagem(produto) {
+            console.log(produto);
+        },
+    },
+};
 </script>
 
 <style scoped>
